0aebb9ea1cd3c97694cd0249bb3536dc7f77c0d9
SeanHsu324 <sean.hsu1071101@gmail.com>
2025-12-06 17:12:07 +0800

    Add files via upload

--- app/main.py
+++ app/main.py
@@ -5,7 +5,7 @@ from PIL import Image
 import colorsys
 import tkinter as tk
 import webbrowser
-from tkinter import colorchooser, filedialog
+from tkinter import colorchooser, filedialog, messagebox
 import json
 from re import search
 from module.download import on_download_button_click, set_main_view
@@ -126,12 +126,16 @@ if os.path.exists(json_file_path):
     with open(json_file_path, "r") as file:
         settings = json.load(file)
         ctk.set_appearance_mode(settings["background_color"])
-        if "download_folder" not in settings:
-            settings["download_folder"] = download_folder
+        if "check_button" not in settings:
+            settings["check_button"] = "True"
+            with open(json_file_path, "w") as file:
+                json.dump(settings, file)
+        if "renew_check_button" not in settings:
+            settings["renew_check_button"] = "True"
             with open(json_file_path, "w") as file:
                 json.dump(settings, file)
 else:
-    settings = {"background_color": "Dark", "subject_color": "#478058", "text_color": "black", "hover_color":"#223E2A", "mp4": "bestvideo[ext=mp4]+bestaudio/mp4", "mp3": "bestaudio/m4a", "download_folder": download_folder}
+    settings = {"background_color": "Dark", "subject_color": "#478058", "text_color": "black", "hover_color":"#223E2A", "mp4": "bestvideo[ext=mp4]+bestaudio/mp4", "mp3": "bestaudio/m4a", "download_folder": download_folder,"check_button":"True","renew_check_button":"True"}
     with open(json_file_path, "w") as file:
         json.dump(settings, file)
 
@@ -247,6 +251,32 @@ def select_download_folder():
         settings["download_folder"] = folder
         with open(json_file_path, "w") as file: json.dump(settings, file)
 
+def choose_Check_value():
+    settings["check_button"] = status_var.get()
+    with open(json_file_path, "w") as file:
+        json.dump(settings, file)
+
+def renew_choose_Check_value():
+    if settings["renew_check_button"]=="True":
+        result = messagebox.askyesno(
+            title="操作確認", 
+            message="確定不「檢查更新」嗎？\n這可能會導致您錯過重要的軟體更新。",
+        )
+        if result:
+            print("使用者點擊了「是」/Yes，繼續執行操作...")
+            settings["renew_check_button"] = renew_status_var.get()
+            with open(json_file_path, "w") as file:
+                json.dump(settings, file)
+
+        else:
+            print("使用者點擊了「否」/No，取消操作。")
+            renew_status_var.set("False" if renew_status_var.get() == "True" else "True")
+    else:
+        settings["renew_check_button"] = renew_status_var.get()
+        with open(json_file_path, "w") as file:
+            json.dump(settings, file)
+
+
 class ToolTip:
     """建立一個提示框，當滑鼠懸停在指定元件上時顯示提示內容"""
     def __init__(self, widget):
@@ -341,6 +371,7 @@ def backhomepag():
     sett_button.place(relx=0.9, rely=0.05)
     wordbutton.pack_forget()
     menu_frame.pack_forget()
+    sett_menu_frame.pack_forget()
     dropdown_menu.pack_forget() 
     Image_quality_menu.pack_forget()
     home_button.place_forget()
@@ -350,6 +381,8 @@ def backhomepag():
     changebutton.pack_forget()
     change_download_folder.pack_forget()
     function_frame.pack_forget()
+    check_button.pack_forget()
+    renew_check_button.pack_forget()
     
 #設定
 def setepag():
@@ -367,9 +400,16 @@ def setepag():
     sett_button.place_forget()
     wordbutton.pack_forget()
     home_button.place(relx=0.05, rely=0.05)
-    backgroundcolor_menu.pack(pady=(50, 20))
-    changebutton.pack(pady=(10, 20))
-    change_download_folder.pack(pady=(10, 20))
+    sett_menu_frame.pack(expand=True, fill="x", pady=30)
+    sett_menu_frame_left.pack(side="left", expand=True, fill="both")
+    sett_menu_frame_right.pack(side="right", expand=True, fill="both")
+    backgroundcolor_menu.pack(side="top", anchor="w",padx=40, pady=(20, 20))
+    changebutton.pack(side="top", anchor="w", padx=40,pady=(10, 20))
+    change_download_folder.pack(side="top", anchor="w",pady=(20, 20))
+    check_button.pack(side="top", anchor="w",pady=(10, 20))
+    renew_check_button.pack(side="top", anchor="w",pady=(10, 20))
+
+
 
 #功能頁
 def function():
@@ -437,12 +477,10 @@ backbutton.pack(side="left", padx=20)
 url_box = ctk.CTkEntry(root, placeholder_text="輸入網址", width=300, height=50, fg_color="transparent", font=("Arial", 20, "bold"))
 menu_frame = ctk.CTkFrame(root, fg_color="transparent")
 dropdown_menu = ctk.CTkOptionMenu(menu_frame, values=["選擇格式", "mp4", "mp3"], command=on_format_change, width=200, height=40, font=("Arial", 20, "bold"))
-dropdown_menu.pack(side="left", padx=10)
 dropdown_menu.set("選擇格式")
 Image_quality_menu = ctk.CTkOptionMenu(menu_frame, values=["最高畫質", "1080p", "720p", "480p", "240p", "144p"], command=set_Image_quality, width=200, height=40, font=("Arial", 20, "bold"))
-Image_quality_menu.pack(side="left", padx=10)
 Image_quality_menu.configure(state="disabled")
-yt_button = ctk.CTkButton(root, text="開始下載", width=100, height=40, corner_radius=40, command=lambda:on_download_button_click(root, url_box, dropdown_menu, ffmpeg_path, first_open, settings["mp3"], settings["mp4"], settings["download_folder"]), font=("Arial", 20, "bold"))
+yt_button = ctk.CTkButton(root, text="開始下載", width=100, height=40, corner_radius=40, command=lambda:on_download_button_click(root, url_box, dropdown_menu, ffmpeg_path, first_open, settings["mp3"], settings["mp4"], settings["download_folder"], settings["check_button"]), font=("Arial", 20, "bold"))
 yt_button.configure(state="disabled")
 home_button = ctk.CTkButton(root, text="home", corner_radius=10, width=40, height=40, command=backhomepag, font=("Arial", 20, "bold"))
 
@@ -461,11 +499,34 @@ if setting_button_icon:
 else:
     sett_button = ctk.CTkButton(root, text="=", corner_radius=10, width=40, height=40, command=setepag, font=("Arial", 20, "bold"))
 sett_button.place(relx=0.9, rely=0.05)
-
-backgroundcolor_menu = ctk.CTkOptionMenu(root, values=["系統", "深色", "淺色"], width=200, height=40, command=set_background_color, font=("Arial", 20, "bold"))
-changebutton = ctk.CTkButton(root, text="選擇顏色", command=choose_color, width=200, height=40, font=("Arial", 20, "bold"))
-change_download_folder = ctk.CTkButton(root, text="變更下載位置", command=select_download_folder, width=200, height=40, font=("Arial", 20, "bold"))
+sett_menu_frame = ctk.CTkFrame(root, fg_color="transparent")
+sett_menu_frame_left = ctk.CTkFrame(sett_menu_frame, fg_color="transparent")
+sett_menu_frame_right = ctk.CTkFrame(sett_menu_frame, fg_color="transparent")
+backgroundcolor_menu = ctk.CTkOptionMenu(sett_menu_frame_left, values=["系統", "深色", "淺色"], width=200, height=40, command=set_background_color, font=("Arial", 20, "bold"))
+changebutton = ctk.CTkButton(sett_menu_frame_left, text="選擇顏色", command=choose_color, width=200, height=40, font=("Arial", 20, "bold"))
+change_download_folder = ctk.CTkButton(sett_menu_frame_right, text="變更下載位置", command=select_download_folder, width=200, height=40, font=("Arial", 20, "bold"))
 ToolTip(change_download_folder) # 提示目前下載位置
+status_var = ctk.StringVar(value=settings["check_button"])
+
+check_button = ctk.CTkCheckBox(
+    sett_menu_frame_right,
+    text="顯示操作確認視窗",
+    variable=status_var,
+    onvalue="True",  # 勾選時的值
+    offvalue="False", # 未勾選時的值
+    command=choose_Check_value,
+    font=("Arial", 16)
+)
+renew_status_var = ctk.StringVar(value=settings["renew_check_button"])
+renew_check_button = ctk.CTkCheckBox(
+    sett_menu_frame_right,
+    text="檢查更新",
+    variable=renew_status_var,
+    onvalue="True",  # 勾選時的值
+    offvalue="False", # 未勾選時的值
+    command=renew_choose_Check_value,
+    font=("Arial", 16)
+)
 
 # 功能頁
 previous_button = ctk.CTkButton(root, text="←", corner_radius=10, width=40, height=40, command=function, font=("Arial", 20, "bold"))
@@ -527,8 +588,9 @@ else:
     print("ctk.set")
 
 first_open = 1
-renew(first_open, root)
-print("renew")
+if settings["renew_check_button"] == "True":
+    renew(first_open, root)
+
 
 
 root.mainloop()
